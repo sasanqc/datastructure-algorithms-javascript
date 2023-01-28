944647d6760fa885b9175585a4f82eedcb18562b
sasanqc <sasanqc@gmail.com>
2023-01-28 16:31:54 +0330

    finding nemo

---
+++ findNemo.js
@@ -0,0 +1,10 @@
+const nemo = ["nemo"];
+function findNmeo(array) {
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === "nemo") {
+      console.log("found nemo");
+    }
+  }
+}
+
+findNmeo(nemo);
